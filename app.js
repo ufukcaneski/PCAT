8da67e328bd2b1207c553a2c6041602078e9eb7b
Ufukcan Eski <ufukcan.b@hotmail.com>
2022-01-02 19:22:08 +0300

    port ayarlamaları ve photo rest'i oluşturuldu.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-    console.log('test');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: 'Photo 1',
+        description: 'This is the first photo',
+    }
+    res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
